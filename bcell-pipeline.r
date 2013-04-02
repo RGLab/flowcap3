ca91eb8dc12b6019bbc67780c80a160a7a4d4959
John Ramey <johnramey@gmail.com>
2013-04-02 08:21:18 -0700

    Updated the B-cell pipeline for Lyoplate 3.0. I have manually updated the Excel files to ensure the B-cell pipeline works properly.

--- bcell-pipeline.r
+++ bcell-pipeline.r
@@ -1,25 +1,37 @@
 library(ProjectTemplate)
 load.project()
 
-gs_file <- "/loc/no-backup/ramey/cytotrol/Bcell/Cytotrol-Bcell.tar"
+path_Lyoplate <- "/loc/no-backup/ramey/Lyoplate/"
+panel <- "Bcell"
 
-# Loads the archived gatingSet object
-gs_bcell <- unarchive(file = gs_file, path = "/loc/no-backup/ramey/cytotrol/Bcell/")
+centers <- dir(path_Lyoplate, include.dirs = TRUE)
+
+# For each center, we construct a GatingSet of FCS files after compensating and
+# transforming the flowSet created from the FCS files in the center's
+# subdirectory under 'path_Lyoplate'.
+gs_list <- lapply(centers, function(center) {
+  message("Center: ", center)
+  path <- file.path(path_Lyoplate, center)
+
+  # The filename of the manually edited Excel file: assumed to be in getwd()
+  xlsx <- dir(pattern = center)
+  comp_matrix <- compensation_lyoplate(path = path, xlsx = xlsx, panel = panel)
+
+  gatingset_lyoplate(path = path, xlsx = xlsx, comp_matrix = comp_matrix,
+                     center = center, panel = panel)
+})
+
+# Creates a GatingSetList and then rbind2 to form a single GatingSet
+gs_bcell <- GatingSetList(gs_list)
+gs_bcell <- rbind2(gs_bcell)
+
+# TODO: Fix error when 'rbind2' is called
+# Error: "The individual flowFrames do not contain identical stains."
 
 # Creates the gating-template object from a CSV file
+# Applies OpenCyto to GatingSet
 gt_csv <- "/loc/no-backup/ramey/cytotrol/Bcell/Cytotrol_Bcell_GatingTemplate.csv"
 gating_template <- gatingTemplate(gt_csv, "Bcell")
 gating(gating_template, gs_bcell, num_nodes = 10, parallel_type = "multicore")
 
-archive(gs_bcell, file = gs_file)
-
-plotGate(gs_bcell, 13, lattice = TRUE, xbin = 128, cond = "factor(Center):factor(Replicate)")
-
-
-Rm("IgD-cd27+", gs_bcell)
-Rm("IgD+cd27+", gs_bcell)
-Rm("IgD+cd27-", gs_bcell)
-Rm("IgD-cd27-", gs_bcell)
-
-
-gating.bsub(gs_bcell, parent = "cd19 & cd20")
+# TODO: Add cytokine prior elicitation to OpenCyto with artificial peaks option
