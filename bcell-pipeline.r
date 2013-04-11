a30e840ab71f8c3d8fe7cf9aeaf4668f0efac0ff
John Ramey <johnramey@gmail.com>
2013-04-11 10:13:30 -0700

    Updated the Excel files for the first 4 centers so that the markers are more straightforward to parse.

--- bcell-pipeline.r
+++ bcell-pipeline.r
@@ -14,47 +14,16 @@ gs_bcell <- unarchive(file = gs_file, path = path_Lyoplate)
 gt_csv <- "gt-bcell.csv"
 gating_template <- gatingTemplate(gt_csv, panel)
 
-# TODO: Ensure that prior arguments are passed to prior_flowClust
-# TODO: Relax the requirement that K must be specified
-
-
-
-fs <- getData(gs_bcell)
-
-
-
-
-trace("gating", signature = c("gtMethod", "GatingSet"), tracer = browser)
-untrace("gating", signature = c("gtMethod", "GatingSet"))
-
-
-set.seed(42)
-prior <- prior_flowClust1d(fs, channel = "FSC-A", min = 0)
-
-
-gates <- fsApply(fs, flowClust.1d, params = "FSC-A", filterId = "nonDebris",
-                                  prior = prior, K = length(prior$Mu0), min = 0)
-sapply(gates, function(x) x@min)
-
-densityplot(~ `FSC-A`, fs, filter = gates)
+gating(gating_template, gs_bcell, num_nodes = 12, parallel_type = "multicore")
 
 
 plotGate(gs_bcell, 2, lattice = TRUE, xbin = 128,
                   cond = "factor(Center):factor(Sample):factor(Replicate)")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# TODO: This should not have to be done manually. OpenCyto should do this.
+# Related: https://github.com/RGLab/openCyto/issues/30
+#  and https://github.com/RGLab/openCyto/issues/31
 # Manually applies more accurate IgD gate that OpenCyto does not currently
 # support.
 Rm("IgD-cd27+", gs_bcell)
@@ -64,5 +33,4 @@ Rm("IgD-cd27-", gs_bcell)
 gating.bsub(gs_bcell, parent = "cd19 & cd20")
 
 
-gating(gating_template, gs_bcell, num_nodes = 12, parallel_type = "multicore")
 archive(gs_bcell, file = gs_file)
